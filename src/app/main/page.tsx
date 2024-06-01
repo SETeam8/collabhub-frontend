d83ce4c639d493f8b6d088494b8301ee61d56027
JiyunIm00 <65935803+JiyunIm00@users.noreply.github.com>
2024-06-01 23:34:42 +0900

    main-page

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,11 +1,22 @@
 import TeamInfo from "./components/TeamInfo";
+import styles from "./main.module.css"
 
 export default function MainPage() {
-  
+    
     return (
-        <>
-            <h1>This is Main page</h1>
+        <div className={styles.container}>
             <TeamInfo></TeamInfo>
-        </>
+            <main className={styles.main}>
+                <section className={styles.section}>
+                    <div className={styles.card}>Welcome greeting</div>
+                </section>
+                <section className={styles.section}>
+                    <div className={styles.card}>Major Schedule</div>
+                </section>
+                <section className={styles.section}>
+                    <div className={styles.card}>Teams Overview</div>
+                </section>
+            </main>
+        </div>
     );
   }
